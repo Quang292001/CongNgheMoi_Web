97327a286edd6bd6da53000fe98cba4ece0be42a
Quang292001 <nh.quang29200189@gmail.com>
2024-03-07 12:30:42 +0700

    Quang-3-signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,22 +6,25 @@ import { AppComponent } from './app.component';
 import { LockscreenComponent } from './components/lockscreen/lockscreen.component';
 import { RecoverpasswordComponent } from './components/recoverpassword/recoverpassword.component';
 import { HomeComponent } from './components/home/home.component';
-import { UserComponent } from './services/user/user.component';
+// import { UserComponent } from './services/user/user.component';
 import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
-import {ReactiveFormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { ImageModule } from 'primeng/image';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { ToastModule } from 'primeng/toast';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MessageService } from 'primeng/api';
 @NgModule({
   declarations: [
     AppComponent,
     LockscreenComponent,
     RecoverpasswordComponent,
     HomeComponent,
-    UserComponent,
+
     LoginComponent,
     SignupComponent
   ],
@@ -32,10 +35,14 @@ import { SignupComponent } from './components/signup/signup.component';
     InputTextModule,
     ReactiveFormsModule,
     ButtonModule,
-    ImageModule
+    ImageModule,
+    FormsModule,
+    HttpClientModule,
+    ToastModule,
+    BrowserAnimationsModule
 
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/user.ts
@@ -0,0 +1,8 @@
+export class User {
+  phonenumber!: number;
+  username!: string;
+  email!: string;
+  password!: string;
+  confirmPassword!:string;
+}
+
