11b0af5a173a6c761355ff183d18478fc56c8ab6
Quang292001 <nh.quang29200189@gmail.com>
2024-03-10 21:47:51 +0700

    Quang commit

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,24 +1,34 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { passwordMatchValidator } from '../../shared/password-match.directive';
+import { User } from '../../user';
+import { SignupService } from '../../services/signup.service';
+import { response } from 'express';
+import { MessageService } from 'primeng/api';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
+  user:User=new User();
   signupForm=this.fb.group({
-    fullname:['',[Validators.required,Validators.pattern(/^[a-zA-Z]+(?: [a-zA-Z]+)*$/)]],
+    fullname:['',[Validators.required]],
     email:['',[Validators.required,Validators.email]],
     password: ['',Validators.required],
-    confirmPassword: ['',Validators.required]
+    confirmPassword: ['',Validators.required],
+    phonenumber:['',Validators.required]
   },{
     validators: passwordMatchValidator
   })
-  constructor(private fb:FormBuilder){}
+  constructor(private fb:FormBuilder,private signupService: SignupService,private messageservice: MessageService,private router: Router){}
 
   get fullname(){
     return this.signupForm.controls['fullname'];
+  }
+  get phonenumber(){
+    return this.signupForm.controls['phonenumber'];
   }
   get email(){
     return this.signupForm.controls['email'];
@@ -28,5 +38,20 @@ export class SignupComponent {
   }
   get confirmPassword(){
     return this.signupForm.controls['confirmPassword'];
+  }
+
+  submitDetails(){
+    const postData = {...this.signupForm.value};
+    delete postData.confirmPassword;
+    this.signupService.signupUser(this.user).subscribe(response=>{
+        console.log(response);
+        this.messageservice.add({severity:'success',summary:'Thành công',detail:'Đăng ký thành công'});
+        this.router.navigate(['login'])
+    },
+    error=>{
+      this.messageservice.add({severity:'error',summary:'Lỗi',detail:'Đăng ký không thành công'});
+    }
+    )
+    console.log(this.user)
   }
 }
---
+++ src/app/services/signup.service.ts
@@ -0,0 +1,18 @@
+// import { SignupService } from './signup.service';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from '../user';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SignupService {
+  private baseUrl="http://localhost:8081/user/signup"//xem lại
+  constructor(private httpClient: HttpClient) {}
+
+  signupUser(user: User):Observable<object>{
+    console.log(user);
+    return this.httpClient.post(`${this.baseUrl}`,user);
+  }
+}
